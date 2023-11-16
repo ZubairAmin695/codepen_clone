4a0495c4b2d3b0691d53d5cebd723fdd8b4889ef
ZubairAmin695 <zubairamin695@gmail.com>
2023-11-16 13:35:05 +0500

    iframe added for the output

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,19 +1,69 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Editor() {
+  const [html, setHtml] = useState("");
 
-    const [html,setHtml]=useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+const [codepenCode,setCodepenCode]=useState("");
 
-    const[css,setCss]=useState("");
-    const[js,setJs]=useState("");
+
+useEffect(()=>{
+    const timeout =setTimeout(()=>{
+        setCodepenCode(
+            `<html>
+            <style>${css}</style>
+            <script>${js}</script>
+            <body>${html}</body>
+          
+            </html>
+            `
+        )
+    },200)
+
+    return ()=>clearTimeout(timeout)
+},[html,css,js])
+
   return (
-     <div>
-        <textarea value={html} type="text" onChange={(e)=>{setHtml(e.target.value)}}/>
+    <div>
+      <div>
+        <textarea
+          value={html}
+          type="text"
+          onChange={(e) => {
+            setHtml(e.target.value);
+          }}
+        />
 
-        <textarea value={css} type="text" onChange={(e)=>{setCss(e.target.value)}}/>
-        <textarea value={js} type="text" onChange={(e)=>{setJs(e.target.value)}}/>
-     </div>
-  )
+        <textarea
+          value={css}
+          type="text"
+          onChange={(e) => {
+            setCss(e.target.value);
+          }}
+        />
+        <textarea
+          value={js}
+          type="text"
+          onChange={(e) => {
+            setJs(e.target.value);
+          }}
+        />
+      </div>
+
+      <div>
+<iframe 
+srcDoc={codepenCode}
+title="output"
+sandbox="allow-scripts"
+frameBorder="0"
+width="100%"
+height="100%"/>
+    
+
+      </div>
+    </div>
+  );
 }
 
-export default Editor
+export default Editor;
